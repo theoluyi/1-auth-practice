f6964155a13ecd7c44a17ac3e3b5f0f76241b0f8
github username <github email address>
2020-04-24 10:46:59 -0400

    got login working again

--- src/App.js
+++ src/App.js
@@ -1,26 +1,103 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Switch, Route} from 'react-router-dom'
+import NavBar from './components/NavBar'
+import Home from './components/Home'
+import Form from './components/Form'
+// import './App.css';
+import ProfileContainer from './ProfileComponents/ProfileContainer'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {withRouter} from 'react-router-dom'
+
+class App extends React.Component {
+  state = {
+    user: {
+      snacks: [],
+      username: "",
+      id: 0
+    },
+    token: ""
+  }
+
+  handleResponse = (resp) => {
+    console.log(resp)
+    if (resp.user) {
+      this.setState({user: resp.user})
+    }
+    // else {return resp.error}
+  }
+
+  componentDidMount() {
+    if (localStorage.getItem("token")) {
+      fetch("http://localhost:4000/persist", {
+        headers: {
+          "Authorization": `Bearer ${localStorage.token}`
+        }
+      })
+      .then(r=> r.json())
+      .then(this.handleResponse)
+
+      // console.log(localStorage.token)
+      // this.setState({token: localStorage.token})
+    }
+  }
+
+  handleLoginSubmit = (userInfo) => {
+    console.log("Login form has been submitted")
+
+    fetch('http://localhost:4000/login', {
+      method: "POST",
+      headers: {
+        "content-type": "application/json"
+      },
+      body: JSON.stringify(userInfo)
+    })
+    .then(r=>r.json())
+    // .then(resp=>console.log(resp))
+    .then(this.handleResponse)
+    // .then(resp=> this.state.user.setState({username: resp.username}))
+  }
+
+  handleRegisterSubmit = (userInfo) => {
+    console.log("Register form has been submitted")
+
+    fetch('http://localhost:4000/users', {
+      method: "POST",
+      headers: {
+        "content-type": "application/json"
+      },
+      body: JSON.stringify(userInfo)
+    })
+    .then(r => r.json())
+    .then(this.handleResponse)
+  }
+
+  renderForm = (routerProps) => {
+    if(routerProps.location.pathname === "/login"){
+      return <Form formName="Login Form" handleSubmit={this.handleLoginSubmit}/>
+    } else if (routerProps.location.pathname === "/register") {
+      return <Form formName="Register Form" handleSubmit={this.handleRegisterSubmit}/>
+    }
+  }
+
+  renderProfile = (routerProps) => {
+    return <ProfileContainer user={this.state.user} addOneSnack={this.addOneSnack} />
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <NavBar/>
+        <Switch>
+          <Route path='/login' render={this.renderForm} />
+          <Route path='/register' render={this.renderForm} />
+          <Route path='/profile' render={this.renderProfile} />
+          <Route path='/' exact render={ () => <Home/> } />
+          <Route render={ () => <p>Page not found</p> } />
+        </Switch>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default withRouter(App);
+// withRouter(boringComp) => magicalComp
